9e225fc14b71c547f8609d76d11a18097932f50e
Kumhar-Harshad <prajapatharshad12@gmail.com>
2024-12-02 22:22:09 +0530

    Add: Add Notification Page

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -7,6 +7,7 @@ import Home from "./Components/Home/Home.jsx";
 import Login from "./Components/Login/Login.jsx";
 import Add_User from './Components/Add User/Add_User.jsx';
 import Add_Group from './Components/Group/AddGroup.jsx';
+import Notification from './Components/Notification/Notification.jsx';
 
 const router = createBrowserRouter(
     createRoutesFromElements(
@@ -15,6 +16,8 @@ const router = createBrowserRouter(
             <Route path='/login' element={<Login/>}/>
             <Route path='/adduser' element={<Add_User/>}/>
             <Route path='/addgroup' element={<Add_Group/>}/>
+            <Route path='/notification' element={<Notification/>}/>
+
         </Route>
     )
 )
