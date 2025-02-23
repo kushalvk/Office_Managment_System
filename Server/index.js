4c8e70d47cd4509f4e87d20090e346570db26e73
kushalvk <kushalvaghela2003@gmail.com>
2025-02-23 21:11:05 +0530

    UPDATE: file Access Permition.

--- Server/index.js
+++ Server/index.js
@@ -23,7 +23,7 @@ const corsOptions = {
   };
 app.use(cors(corsOptions));
 app.use(express.json());
-app.use(express.static("uplodes"))
+app.use('/uploads', express.static("uploads"));
 
 if (mongoose.connect(process.env.MONGODB_URL)) {
     console.log("Prime_Hub Database connected");
