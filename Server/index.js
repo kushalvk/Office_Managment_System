6ddfee746f9c5a203867e3aab6eade30d2214291
kushalvk <kushalvaghela2003@gmail.com>
2025-02-23 22:50:33 +0530

    UPDATE: Limit File size.

--- Server/index.js
+++ Server/index.js
@@ -25,6 +25,8 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/uplodes', express.static(path.join(__dirname, "uplodes")));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 if (mongoose.connect(process.env.MONGODB_URL)) {
     console.log("Prime_Hub Database connected");
