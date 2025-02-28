ffa6285dd3084f6a9b816d3559b9c611cb5b0323
kushalvk <kushalvaghela2003@gmail.com>
2025-02-28 11:15:12 +0530

    ADD: handle error to large file size at report.

--- Server/routers/Report_Route.js
+++ Server/routers/Report_Route.js
@@ -4,16 +4,18 @@ const {
     newlyAddedReportsController, pendingApprovalController
 } = require("../controllers/Report_Controller");
 const {default: upload} = require("../middlewares/uploadMiddleware");
+const cors = require("cors");
+require("dotenv").config();
 
 const router = express.Router()
 
-router.post('/add-report', (req, res, next) => {
+router.post('/add-report', cors({ origin: process.env.FRONT_URL }), (req, res, next) => {
     upload.single("reportdocument")(req, res, (err) => {
         if (err) {
             if (err.code === "LIMIT_FILE_SIZE") {
-                return res.status(400).json({message: "File too large. Maximum allowed size is 10MB."});
+                return res.status(400).json({ message: "File too large. Maximum allowed size is 10MB." });
             }
-            return res.status(500).json({message: "File upload error", error: err.message});
+            return res.status(500).json({ message: "File upload error", error: err.message });
         }
         next();
     });
