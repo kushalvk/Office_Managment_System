d2fc2b9f30a49e8bf17493a9c1bc6ea79b8061ed
kushalvk <kushalvaghela2003@gmail.com>
2025-03-01 19:18:56 +0530

    UPDATE: Final Commit.

--- Client/src/Components/Login/Login.jsx
+++ Client/src/Components/Login/Login.jsx
@@ -106,20 +106,6 @@ function Login() {
                             </div>
                         </div>
                         <div className="flex items-center justify-between">
-                            <div className="flex items-center">
-                                <input
-                                    id="remember-me"
-                                    name="remember-me"
-                                    type="checkbox"
-                                    className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                                />
-                                <label
-                                    htmlFor="remember-me"
-                                    className="ml-2 block text-sm text-gray-900"
-                                >
-                                    Remember me
-                                </label>
-                            </div>
                             <div className="text-sm">
                                 <a
                                     href="/forgot-password"
@@ -141,7 +127,7 @@ function Login() {
                     <p className="mt-10 text-center text-sm text-gray-500">
                         Don’t have an account?{' '}
                         <a
-                            href="#"
+                            href="/contactus"
                             className="font-medium text-indigo-600 hover:text-indigo-500"
                         >
                             Talk to Your Manager
